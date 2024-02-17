ab682df68e3f7ead6a2cbedb3ed92f966571bee9
John Pinto <jwpinto2@gmail.com>
2024-02-17 00:06:10 +0000

    messageservice

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -2,8 +2,17 @@ package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.example.entity.Message;
+import java.util.List;
 
 @Repository
-public interface MessageRepository extends JpaRepository<Message, Integer> {
+public interface MessageRepository extends JpaRepository<Message, Integer>{
+    Message findById(int id);
+    Message deleteById(int id);
+    
+    @Query("FROM Message where posted_by = :posted_by")
+    List<Message> findAllByPostedBy(@Param(value = "posted_by") int id);
+
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,53 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+import java.util.List;
+
+@Service
 public class MessageService {
+    @Autowired
+    public MessageRepository messageRepository;
+
+    public Message postMessage(Message message) {
+
+            return messageRepository.save(message);
+    }
+
+    public List<Message> getAllMessages() {
+        List<Message> messages = messageRepository.findAll();
+        return messages;
+    }
+
+    public Message getMessageById(int id) {
+        Message message = messageRepository.findById(id);
+        if (message != null) return message;
+        else return null;
+    }
+
+    public List<Message> getMessageByUser(int id){
+        List<Message> list = messageRepository.findAllByPostedBy(id);
+        if(list != null) return list;
+        else return null;
+    }
+
+    public Message deleteMessage(int id){
+        Message message = messageRepository.findById(id);
+        if(message != null) {
+            messageRepository.deleteById(id);
+            return message;
+        }
+        else return null;
+    }
+
+    public Message updateMessage(Message message, int id){
+        Message first = messageRepository.findById(id);
+        if(first != null && message.getMessage_text() != "" && message.getMessage_text().length() < 255){
+            first.setMessage_text(message.getMessage_text());
+            return messageRepository.save(first);
+        }
+        else return null;
+    }
 }
